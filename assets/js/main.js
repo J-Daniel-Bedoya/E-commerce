238ad4f568844def4aea90f71d1edeac83532def
daniel1718 <jbedoyachavarriaga@gmail.com>
2022-08-16 17:59:17 -0500

    Nueva funcionalidad

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,33 @@
+const productosStore = [
+    {
+        nombre: "Hoodies", 
+        precio: "$ 14.00", 
+        img: 'https://i.ibb.co/S7dPp5D/featured1.png',
+        stock: "10",
+        id: 1, 
+    },
+                        
+    {
+        nombre: "Shirts", 
+        precio: "$ 15.00", 
+        img: 'https://i.ibb.co/QNHZd4K/featured2.png',
+        stock: "15",
+        id: 2, 
+    },
+                         
+    {
+        nombre: "Sweatshirts", 
+        precio: "$ 12.00", 
+        img: 'https://i.ibb.co/R7CZn5n/featured3.png',
+        stock: "20",
+        id: 3, 
+    },
+                   
+];
 
 document.addEventListener( "DOMContentLoaded", () =>{
     load()
-    icorporarProductos();
+    icorporarProductos(productosStore);
 })
 
 /* =========== LOADER ========== */
@@ -50,61 +76,58 @@ cartOpen.addEventListener( "click", () => {
 
 cartClose.addEventListener( "click", () => {
     cartContainer.classList.add( "hide" )
-})
+});
 
 
 
 /* ========SCROLL========= */
 const header = document.getElementById("header")
+const b1 = document.querySelector(".bxs1")
+const b2 = document.querySelector(".bxs2")
+const b3 = document.querySelector(".bxs3")
+const b4 = document.querySelector(".bxs4")
 
 window.addEventListener( "scroll", () =>{
-    if( window.scrollY >= 50 ){
-        header.classList.add("scroll-header")
-    }else{
-        header.classList.remove("scroll-header")
-    }
+    // setTimeout(() => {
+        if( window.scrollY >= 50 ){
+            header.classList.add("scroll-header")
+            b1.classList.add("bxs1")
+            b2.classList.add("bxs2")
+            b3.classList.add("bxs3")
+            b4.classList.add("bxs4")
+        }else{
+            header.classList.remove("scroll-header")
+            b1.classList.remove("bxs1")
+            b2.classList.remove("bxs2")
+            b3.classList.remove("bxs3")
+            b4.classList.remove("bxs4")
+        }
+    // }, 4);
 })
 
 
 /* ==================== PRODUCTOS ==================== */
-const productos = [
-    {
-        nombre: "Buso Rojo", 
-        precio: "$ 14.00", 
-        img: './images/featured1.png',
-        id: 1, 
-    },
-                        
-    {
-        nombre: "Buso Negro", 
-        precio: "$ 15.00", 
-        img: './images/featured2.png',
-        id: 2, 
-    },
-                         
-    {
-        nombre: "Buso Blanco", 
-        precio: "$ 12.00", 
-        img: './images/featured3.png',
-        id: 3, 
-    },
-                   
-];
 
 
 
-const cardProductos= document.getElementById("productos__card sudaderas")
 
-function icorporarProductos(){
-    productos.forEach(products => {
-        const card = document.createElement("div")
-        card.classList.add("card")
-        card.innerHTML = `
-            <img src="${products.img}" alt="${products.nombre}">
-            <h2>${products.nombre}</h2>
-            <h3>${products.precio}</h3>
-            <button class="btn-add-cart" data-id="${products.id}">Agregar al carrito</button>
-        `
-        cardProductos.appendChild(card)
-    } )
+const cardProductos= document.querySelector(".productos__card")
+
+function icorporarProductos(productosFet){
+    let card = ``;
+    productosFet.map(products => {
+        card += `<div class="cards">
+            <div class="card__img">
+                <img src=${products.img} class="img-products" alt="${products.nombre}">
+            </div>
+            <div class="card__info">
+                <h2>${products.precio}</h2> 
+                <p>Stock: ${products.stock}</p>
+                <h3>${products.nombre}</h3>
+                <button class="btn-add-cart" data-id="${products.id}">+</button>
+            </div>
+        </div>`
+    });
+    cardProductos.innerHTML = card;
 }
+
